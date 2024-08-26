69c46044314f2b663a189284da6fb932305c4b1f
habibulloh004 <habibulloh4080@gmail.com>
2024-08-27 00:31:31 +0500

    im tired

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { useState, useEffect, lazy, Suspense, useLayoutEffect } from "react";
 import Loader from "./components/Loader/Loader";
